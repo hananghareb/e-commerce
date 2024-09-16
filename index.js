c409eef3b2c6a7c0a8884c960b2efd9f3313808b
Hanan <hananghareb@>
2024-09-16 19:16:09 +0200

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 dotenv.config();
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.port || 3000
 app.use(cors())
 app.use(express.json())
 app.use('/uploads',express.static('uploads'))
